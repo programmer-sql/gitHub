f76dfe5685a9e3da5acb5111b37ee2e67754a006
programmer-sql <66375092+programmer-sql@users.noreply.github.com>
2024-09-21 14:10:32 -0700

    Added New

---
+++ Scripts/Working/payment Table Tableau Sample.sql
@@ -0,0 +1,27 @@
+CREATE TABLE payments
+(
+    pymtDate            date,
+    paymentCompany      varchar,
+    paymentHead         varchar,
+    paymentSubHead      varchar,
+    paymentAmt          decimal(30, 4)
+);
+CREATE TABLE date_dimension
+(
+    valuekey             integer,
+    datekey              Date
+);
+
+INSERT INTO date_dimension
+SELECT seq4() + 1 valueKey, dateadd(day, -1 * valueKey, to_date('2025-12-31'))
+FROM TABLE(GENERATOR(ROWCOUNT => 3000));
+INSERT INTO payments
+SELECT pymtDate , paymentCompany, paymentHead, paymentSubHead, UNIFORM(10, 155, RANDOM())
+FROM
+(
+    SELECT dateadd(day, -1 * (seq4() + 1), current_date()) pymtDate
+    FROM TABLE(GENERATOR(ROWCOUNT => 56))
+)
+Cross JOIN values ('Digtal Co'), ('Navigator LLC'), ('Somaya INC.') PC(paymentCompany)
+Cross JOIN values ('Digtal'), ('Cash'), ('Cheque') PH(paymentHead)
+Cross JOIN values ('Full'), ('Partial') PS(paymentSubHead);
