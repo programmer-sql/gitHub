f1ece5ca96b2dced9df4b3b7230a3422aaf59924
programmer-sql <66375092+programmer-sql@users.noreply.github.com>
2024-09-28 23:21:51 -0700

    Added new File

---
+++ Scripts/Working/unPivot and Pivot Part 3 (ARRAY_CONSTRUCT).sql
@@ -0,0 +1,67 @@
+Select * 
+from sampledb.testuser.attendanceCount 
+WHERE 1 = 1
+AND attendancedate = to_date('2024-09-06 00:00:00.000')
+Fetch 10;
+
+
+Select 
+Pvt.VALUE: CLASS::VARCHAR AS CALSS,
+Pvt.VALUE: SEC_1::INT AS SEC_1, 
+Pvt.VALUE: SEC_2::INT AS SEC_2, 
+Pvt.VALUE: SEC_3::INT AS SEC_3, 
+Pvt.VALUE: SEC_4::INT AS SEC_4, 
+Pvt.VALUE: SEC_5::INT AS SEC_5 
+from sampledb.testuser.attendanceCount hrd,
+TABLE(FLATTEN(INPUT => ARRAY_CONSTRUCT(
+    OBJECT_CONSTRUCT_KEEP_NULL(
+    'CLASS', 'Class 1',
+    'SEC_1', hrd.CLASS_1_SEC_1,
+    'SEC_2', hrd.CLASS_1_SEC_2,
+    'SEC_3', hrd.CLASS_1_SEC_3,
+    'SEC_4', hrd.CLASS_1_SEC_4,
+    'SEC_5', NULL
+    ),
+    OBJECT_CONSTRUCT_KEEP_NULL(
+    'CLASS', 'Class 2',
+    'SEC_1', hrd.CLASS_2_SEC_1,
+    'SEC_2', hrd.CLASS_2_SEC_2,
+    'SEC_3', hrd.CLASS_2_SEC_3,
+    'SEC_4', hrd.CLASS_2_SEC_4,
+    'SEC_5', NULL
+    )
+    ))) AS Pvt
+WHERE 1 = 1
+AND attendancedate = to_date('2024-09-06 00:00:00.000')
+Fetch 10;
+
+/* ATTENDANCEDATE
+CLASS_1_SEC_1
+CLASS_1_SEC_2
+CLASS_1_SEC_3
+CLASS_1_SEC_4
+CLASS_2_SEC_1
+CLASS_2_SEC_2
+CLASS_2_SEC_3
+CLASS_2_SEC_4
+CLASS_3_SEC_1
+CLASS_3_SEC_2
+CLASS_3_SEC_3
+CLASS_3_SEC_4
+CLASS_4_SEC_1
+CLASS_4_SEC_2
+CLASS_4_SEC_3
+CLASS_4_SEC_4
+CLASS_5_SEC_1
+CLASS_5_SEC_2
+CLASS_5_SEC_3
+CLASS_6_SEC_1
+CLASS_6_SEC_2
+CLASS_6_SEC_3
+CLASS_6_SEC_4
+CLASS_6_SEC_5
+CLASS_6_SEC_6
+CLASS_6_SEC_7
+CLASS_6_SEC_8 
+Select GET_DDL('table', 'sampledb.testuser.attendanceCount')
+*/
